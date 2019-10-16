847416b7d7f12ecded2be8b026d957cd84a22901
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-10-16 11:05:28 +0300

    [SHARED] Load material modules

--- src/app/features/movie/movie.module.ts
+++ src/app/features/movie/movie.module.ts
@@ -1,14 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { environment } from 'src/environments/environment';
+import { HeaderComponent } from './components/header/header.component';
+import { MovieRootComponent } from './containers/movie-root/movie-root.component';
 import { MovieRoutingModule } from './movie-routing.module';
 import { MovieService } from './services/movie.service';
 import { MovieMockService } from './services/movie.service.mock';
-import { MovieRootComponent } from './containers/movie-root/movie-root.component';
+import { UserContentComponent } from './components/user-content/user-content.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [MovieRootComponent],
-  imports: [CommonModule, MovieRoutingModule],
+  declarations: [MovieRootComponent, HeaderComponent, UserContentComponent, FooterComponent],
+  imports: [CommonModule, MovieRoutingModule, SharedModule],
   providers: [
     {
       provide: MovieService,
