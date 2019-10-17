5e942dd4e76626870a8f4218b0c6ba830fd7b097
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-10-17 14:58:00 +0300

    [MOVIE] refactor active class

--- src/app/features/movie/movie-routing.module.ts
+++ src/app/features/movie/movie-routing.module.ts
@@ -3,14 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { MovieRootComponent } from './containers/movie-root/movie-root.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: MovieRootComponent,
-  },
+    {
+        path: 'movies',
+        component: MovieRootComponent,
+    },
+    {
+        path: 'favorites',
+        component: MovieRootComponent,
+    },
+    {
+        path: '',
+        redirectTo: 'movies',
+        pathMatch: 'full',
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
 export class MovieRoutingModule {}
