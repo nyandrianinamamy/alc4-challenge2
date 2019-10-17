ea56485aa04f9c35695a71ed51555a05f5e242c6
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-10-17 09:07:01 +0300

    [MOVIE] create basic grid layout

--- src/app/features/movie/containers/movie-root/movie-root.component.ts
+++ src/app/features/movie/containers/movie-root/movie-root.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-movie-root',
   templateUrl: './movie-root.component.html',
-  styleUrls: ['./movie-root.component.scss']
+  styleUrls: ['./movie-root.component.scss'],
 })
 export class MovieRootComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    /* */
   }
 
+  ngOnInit() {}
 }
--- src/app/features/movie/movie.module.ts
+++ src/app/features/movie/movie.module.ts
@@ -2,16 +2,14 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { environment } from 'src/environments/environment';
-import { HeaderComponent } from './components/header/header.component';
+import { UserContentComponent } from './components/user-content/user-content.component';
 import { MovieRootComponent } from './containers/movie-root/movie-root.component';
 import { MovieRoutingModule } from './movie-routing.module';
 import { MovieService } from './services/movie.service';
 import { MovieMockService } from './services/movie.service.mock';
-import { UserContentComponent } from './components/user-content/user-content.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [MovieRootComponent, HeaderComponent, UserContentComponent, FooterComponent],
+  declarations: [MovieRootComponent, UserContentComponent],
   imports: [CommonModule, MovieRoutingModule, SharedModule],
   providers: [
     {
